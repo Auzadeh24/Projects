95270aa400003f69f34149f87930de82d8f24427
auzadeh7049 <150835470+auzadeh7049@users.noreply.github.com>
2024-02-04 15:31:15 -0500

    Delete project2 directory

--- project2/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-vpc_name       = "project2_vpc"
-vpc_cidr_block = "10.0.0.0/16"
-
--- project2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_name" {
-  description = "vpc name"
-}
-variable "vpc_cidr_block" {
-  description = "cidr block for vpc"
-}
-
