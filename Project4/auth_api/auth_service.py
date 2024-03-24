a9fcaec008c36bda5aeba7b99ccf5ef4ea15ded9
Auzadeh <auzadeh.samani@gmail.com>
2024-03-24 12:42:46 -0400

    Create auth_service.py

---
+++ Project4/auth_api/auth_service.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/register", methods=["POST"])
+def register_user():
+
+    return jsonify({"message": "User registered successfully"})
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+
+    return jsonify({"message": "User logged in successfully"})
+
+
+@app.route("/reset-password", methods=["POST"])
+def reset_password():
+
+    return jsonify({"message": "Password reset successful"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
